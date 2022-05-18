66906d21bf958cc25aa4186eca4661e8c48e01e8
Felipe Machado <f_nmachado@hotmail.com>
2022-05-18 13:24:51 -0300

    Adding test main

---
+++ src/teste/NSequencial.java
@@ -0,0 +1,83 @@
+package teste;
+
+import ed.nseq.Deque;
+import ed.nseq.Fila;
+import ed.nseq.ListaLigada;
+import ed.nseq.Pilha;
+
+public class NSequencial {
+    public static void main(String[] args) {
+    /*
+        Pilha pi = new Pilha();
+        pi.adicionar(1);
+        pi.adicionar(2.0);
+        pi.adicionar("abc");
+        System.out.println("Comprimento: "+pi.comprimento());
+        System.out.println("Primeiro Elemento: "+pi.elemento(0));
+        System.out.println("Pilha: "+pi.toString());
+        do{
+            pi.remover();
+            System.out.println("-1: \t"+pi.toString());
+        }while(!pi.vazia());
+    */
+
+    /*
+        Fila fi = new Fila();
+        fi.adicionar(1);
+        fi.adicionar(2.0);
+        fi.adicionar("abc");
+        System.out.println("Comprimento: "+fi.comprimento());
+        System.out.println("Primeiro Elemento: "+fi.elemento(0));
+        System.out.println("Fila: "+fi.toString());
+        do{
+            fi.remover();
+            System.out.println("-1: \t"+fi.toString());
+        }while(!fi.vazia());
+    */
+
+    /*
+        ListaLigada li = new ListaLigada();
+        li.adicionar(1);
+        li.adicionar(2.0);
+        li.adicionar("abc");
+        li.inserir(0,true);
+        System.out.println("Comprimento: "+li.comprimento());
+        System.out.println("Primeiro Elemento: "+li.elemento(0));
+        System.out.println("Lista: "+li.toString());
+        do{
+            int i=0;
+            li.remover(i);
+            System.out.println("-1: \t"+li.toString());
+            i++;
+        }while(!li.vazia());
+    */
+    /*
+        Deque de = new Deque();
+        de.adicionarInicio(2.0);
+        de.adicionarFim("abc");
+        de.adicionarInicio(1);
+        System.out.println("Comprimento: "+de.comprimento());
+        System.out.println("Primeiro Elemento: "+de.elemento(0));
+        System.out.println("REMOVENDO DO INICIO");
+        System.out.println("Deque: "+de.toString());
+        do{
+            int i=0;
+            de.removerInicio();
+            System.out.println("-1: \t"+de.toString());
+            i++;
+        }while(!de.vazia());
+        de.adicionarInicio(2.0);
+        de.adicionarFim("abc");
+        de.adicionarInicio(1);
+        System.out.println("REMOVENDO DO FIM");
+        System.out.println("Deque: "+de.toString());
+        do{
+            int i=0;
+            de.removerFim();
+            System.out.println("-1: \t"+de.toString());
+            i++;
+        }while(!de.vazia());
+    */
+    }
+
+}
---
+++ src/teste/Sequencial.java
@@ -0,0 +1,62 @@
+package teste;
+
+import ed.seq.Fila;
+import ed.seq.Lista;
+import ed.seq.Pilha;
+
+public class Sequencial {
+    public static void main(String[] args) {
+
+    /*
+        //Pilha
+        Pilha pi = new Pilha(5);
+        pi.adicionar(1);
+        pi.adicionar(2.0);
+        pi.adicionar("abc");
+        System.out.println("Capacidade: "+pi.capacidade());
+        System.out.println("Comprimento: "+pi.comprimento());
+        System.out.println("Primeiro Elemento: "+pi.elemento(0));
+        System.out.println("Pilha: "+pi.toString());
+        do{
+            pi.remover();
+            System.out.println("-1: \t"+pi.toString());
+        }while(!pi.vazia());
+    */
+
+    /*
+        //Fila
+        Fila fi = new Fila(5);
+        fi.adicionar(1);
+        fi.adicionar(2.0);
+        fi.adicionar("abc");
+        System.out.println("Capacidade: "+fi.capacidade());
+        System.out.println("Comprimento: "+fi.comprimento());
+        System.out.println("Primeiro Elemento: "+fi.elemento(0));
+        System.out.println("Fila: "+fi.toString());
+        do{
+            fi.remover();
+            System.out.println("-1: \t"+fi.toString());
+        }while(!fi.vazia());
+    */
+    /*
+        //Lista
+        Lista li = new Lista(5);
+        li.adicionar(1);
+        li.adicionar(2.0);
+        li.adicionar("abc");
+        li.inserir(0,true);
+        System.out.println("Capacidade: "+li.capacidade());
+        System.out.println("Comprimento: "+li.comprimento());
+        System.out.println("Primeiro Elemento: "+li.elemento(0));
+        System.out.println("Lista: "+li.toString());
+        do{
+            int i=0;
+            li.remover(i);
+            System.out.println("-1: \t"+li.toString());
+            i++;
+        }while(!li.vazia());
+     */
+
+    }
+
+}
