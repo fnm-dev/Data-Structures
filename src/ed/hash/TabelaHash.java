f4bb68caa42f21074d1dd193974356a1b704bc92
Felipe <f_nmachado@hotmail.com>
2022-06-12 19:48:14 -0300

    Update TabelaHash.java

--- src/ed/hash/TabelaHash.java
+++ src/ed/hash/TabelaHash.java
@@ -1,7 +1,8 @@
 package ed.hash;
 
 public class TabelaHash {
-    private int size = 0;
+    public int size = 0;
+    public int collision = 0;
     public NodoInterno[] bucket;
 
     public TabelaHash() {
@@ -26,43 +27,92 @@ public class TabelaHash {
             bucket[hash].value = value;
             bucket[hash].next = null;
             size++;
-        } else{
-            System.out.println("Colisão");
-            NodoInterno aux= bucket[hash];
-            while(!aux.key.equals(key) && aux.next!= null){
-                aux= aux.next;
+        } else {
+            collision++;
+            NodoInterno aux = bucket[hash];
+            while (!aux.key.equals(key) && aux.next != null) {
+                aux = aux.next;
             }
         }
     }
 
-    public Object get(Object key){
-        int hash=(key.hashCode() & 0x7fffffff) % bucket.length;
-        Object value= null;
-        if(bucket[hash] != null){
-            NodoInterno aux= bucket[hash];
-            while(!aux.key.equals(key) && aux.next!= null){
-                aux= aux.next;
+    public Object get(Object key) {
+        int hash = (key.hashCode() & 0x7fffffff) % bucket.length;
+        Object value = null;
+        if (bucket[hash] != null) {
+            NodoInterno aux = bucket[hash];
+            while (!aux.key.equals(key) && aux.next != null) {
+                aux = aux.next;
             }
-            if(aux.key.equals(key)){
-                value= aux.value;
+            if (aux.key.equals(key)) {
+                value = aux.value;
             }
         }
         return value;
     }
 
-    public Object remove(Object key){
-        int hash=(key.hashCode() & 0x7fffffff) % bucket.length;
-        Object value= null;
-        if(bucket[hash] != null){
-            NodoInterno aux= bucket[hash];
-            NodoInterno ant= null;
-            while(!aux.key.equals(key) && aux.next!= null){
-                ant= aux;aux= aux.next;
+    public Object remove(Object key) {
+        int hash = (key.hashCode() & 0x7fffffff) % bucket.length;
+        Object value = null;
+        if (bucket[hash] != null) {
+            NodoInterno aux = bucket[hash];
+            NodoInterno ant = null;
+            while (!aux.key.equals(key) && aux.next != null) {
+                ant = aux;
+                aux = aux.next;
             }
-            if(aux.key.equals(key)){
-                value= aux.value;ant= aux.next;aux= null;
+            if (aux.key.equals(key)) {
+                value = aux.value;
+                ant = aux.next;
+                aux = null;
             }
         }
         return value;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean containsKey(Object key) {
+        int hash = (key.hashCode() & 0x7fffffff) % bucket.length;
+        if (bucket[hash] != null) {
+            NodoInterno aux = bucket[hash];
+            while (!aux.key.equals(key) && aux.next != null){
+                aux = aux.next;
+            }
+            if (aux.key.equals(key)) return true;
+        }
+        return false;
+    }
+
+    public boolean containsValue(Object value){
+        for(int i=0; i<bucket.length; i++){
+            for(NodoInterno aux = bucket[i]; aux!=null; aux=aux.next){
+                if(value.equals(aux.value)) return true;
+            }
+        }
+        return false;
+    }
+
+    public double collisionRate(){
+        double average = collision/size;
+        return average;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Tabela Hash: [ ");
+        for(int i=0; i<bucket.length; i++){
+            for(NodoInterno aux = bucket[i]; aux!=null; aux=aux.next){
+                if(aux!=null) {
+                    sb.append("("+aux.key + "|" + aux.value+")");
+                    sb.append(" ");
+                }
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
+
